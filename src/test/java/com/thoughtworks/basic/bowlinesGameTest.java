bed33059e0ee4bf072f4fbe6f67df573a2631b3e
zxl0526 <851450219@qq.com>
2020-07-20 21:43:41 +0800

    should_return_true_when_given_roll_20_times

--- src/test/java/com/thoughtworks/basic/bowlinesGameTest.java
+++ src/test/java/com/thoughtworks/basic/bowlinesGameTest.java
@@ -34,5 +34,34 @@ public class bowlinesGameTest {
         assertEquals(10, bowlingGame.getScore());
         assertEquals("5|5", bowlingGame.getRecord());
     }
+    @Test
+    public void should_return_false_when_given_roll_4_times() {
+        //given
+        BowlingGame bowlingGame = new BowlingGame(10, 2);
+
+        //when
+        bowlingGame.roll(0);
+        bowlingGame.roll(5);
+        bowlingGame.roll(0);
+        bowlingGame.roll(5);
+
+        //then
+        Assert.assertFalse(bowlingGame.isEnd());
+    }
+
+    @Test
+    public void should_return_true_when_given_roll_20_times() {
+        //given
+        BowlingGame bowlingGame = new BowlingGame(10, 2);
+
+        //when
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+
+        //then
+        Assert.assertTrue(bowlingGame.isEnd());
+    }
+
 
 }
