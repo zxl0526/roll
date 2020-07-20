2d834c38f9286f0581d9c93fbaeb37d5b73a294e
zxl0526 <851450219@qq.com>
2020-07-20 21:42:26 +0800

    should_return_10_when_given_roll_0_roll_5_roll_0_roll_5

--- src/test/java/com/thoughtworks/basic/bowlinesGameTest.java
+++ src/test/java/com/thoughtworks/basic/bowlinesGameTest.java
@@ -18,6 +18,21 @@ public class bowlinesGameTest {
         assertEquals(5, bowlingGame.getScore());
         assertEquals("5", bowlingGame.getRecord());
     }
+    @Test
+    public void should_return_10_when_given_roll_0_roll_5_roll_0_roll_5() {
+        //given
+        BowlingGame bowlingGame = new BowlingGame(10, 2);
 
+        //when
+        bowlingGame.roll(0);
+        bowlingGame.roll(5);
+        bowlingGame.roll(0);
+        bowlingGame.roll(5);
+
+
+        //then
+        assertEquals(10, bowlingGame.getScore());
+        assertEquals("5|5", bowlingGame.getRecord());
+    }
 
 }
